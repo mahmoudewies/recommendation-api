d06a977e2a58f8a2cb800f7341e7cf1f1527e45c
mahmoud ewis <mahmoudewis.ai@gmail.com>
2025-04-26 13:10:09 +0300

    test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
-
+import os
 app = Flask(__name__)
 
 model = joblib.load("recommender_model.pkl")
@@ -62,5 +62,7 @@ def recommend():
         return jsonify({"error": str(e)}), 500
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
